b014333a7ae6ad0fd2316d95810e369136923e43
UlyssesWu <wdwxy12345@gmail.com>
2021-12-05 22:15:19 +0800

    init

---
+++ MahAppsTest/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using MahAppsTest.Annotations;
+
+namespace MahAppsTest
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Desc { get; set; }
+    }
+
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public List<Character> Data { get; set; } = new List<Character>()
+        {
+            new Character()
+            {
+                Name = "Klee", Gender = "Female", Desc = "Red"
+            },
+
+            new Character()
+            {
+                Name = "Xingqiu", Gender = "Male", Desc = "Blue"
+            }
+        };
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MahAppsTest/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using ControlzEx.Theming;
+
+namespace MahAppsTest
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var vm = new MainViewModel();
+            DataContext = vm;
+            InitializeComponent();
+            CharacterDataGrid.ItemsSource = vm.Data;
+        }
+
+        private void ThemeToggled(object sender, RoutedEventArgs e)
+        {
+            var theme = ThemeManager.Current.DetectTheme();
+            if (theme != null)
+            {
+                ThemeManager.Current.ChangeTheme(Application.Current, ThemeManager.Current.GetInverseTheme(theme));
+            }
+        }
+
+        private void QuickLoad(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Title = DateTime.Now.ToString("s");
+        }
+    }
+}
